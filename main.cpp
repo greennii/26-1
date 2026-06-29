ca71a2679a509e2e8b20483d5b07362efcd26dde
greennii <brilliantstar@ewha.ac.kr>
2026-06-29 06:05:14 +0000

    lab1

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+int main()
+ { 
+
+     
+//    int i{1};
+//    const int end{ 10 };
+//    do{
+//        std::cout << i << ' '; ++i;
+
+//    }while { i <= end};
+//    std::cout << std::endl;
+//    i = 1;
+//    while ( i <= end)
+//    {
+//     std::cout << i << ' '; ++i;
+//    }std::cout << std::endl;
+   
+
+//    for ( i = 1 ; i <= end; ++i)
+//    {
+//     std::cout << i << ' '; ++i;
+//    }std::cout << std::endl;
+   
+
+
+
+
+
+//std::cout <<"Enter your score (0~100):";
+//     int score{};
+//     char grade{};
+//     std::cin >> score;
+//     if(score >= 90) grade = 'A';
+//     else if(score >= 80) grade = 'B';
+//     else if(score >= 70) grade = 'C';
+//     else if(score >= 60) grade = 'D';
+//     else grade = 'F';
+
+    // switch (score/10)  
+    // {
+    // case 10:
+    // case 9: grade = 'a;' break;
+    // case 8: grade = 'b;'  break;
+    // case 7: grade = 'c;'  break;
+    // case 6: grade = 'd;'  break;
+    // default: grade = 'f'
+    //     break;
+    // } std::cout << "Your grade is " << grade << std::endl;
+
+
+
+    // std:::cout << "Enter 1,2 or 3:";
+
+    // int x{};
+    // std::cout >> x;
+    // switch(x)
+    // {
+    //     case 1 : std::cout << "one\n"; break;
+    //     case 2 : std::cout << "two\n"; break;
+    //     case 3 : std::cout << "three\n"; break;
+    //     case 4 : std::cout << "unknown\n"; break;
+    //     default : std::cout << "unknown\n";
+    // }
+      
+//    std::cout << "Enter two integer: ";
+//    int x{}, y{}, z{};
+//    std::cin >> x>> y;
+//    //if (x > y) z = x;
+//    //else z =y;
+//    z = ( x> y) ?x: y;
+//    std::cout << z<< std::endl;4
+
+    
+
+
+//   int n{2};
+
+//   std::cout << 2 * (n++) << '\n';
+//   std::cout << n << std::endl;
+
+//   int m{2};
+//    std::cout << 2 * (++m) << '\n';
+//   std::cout << n << std::endl; 
+
+// int numOfstudents { 30 }; numOfstudents = 20 ;
+// std::cout <<  numOfstudents << std::endl;
+// std::cout << sizeof( numOfstudents ) << std::endl;
+// std::cout << typeid( numOfstudents ).name() << std::endl;
+// std::cout << typeid(static_cast<double>( numOfstudents )).name() << std::endl;       
+
+return 0;
+
+}
